0c0bc3881e16422908e8e6b23331a25f9f9a1c0a
Assalli13 <124143755+Assalli13@users.noreply.github.com>
2023-02-17 13:31:20 +0100

    Update test_daschboard.py

--- test_daschboard.py
+++ test_daschboard.py
@@ -238,12 +238,10 @@ def var(data):
     var3 = st.selectbox('var3', data.columns)
         #st.write(var3)
     return var1, var2, var3
+
  if(st.button('select_var')):
     var1, var2, var3 = var(data_test)
-    if var1 and var2 and var3:
-        bivariate_analysis(data, var1, var2, var3)
-    else:
-        st.write("Veuillez sélectionner une valeur pour chaque variable")
+    bivariate_analysis(data, var1, var2, var3)
 #st.write('les variables sélectionner sont :' , var1, var2, var3)
 #if(st.button(' bivariate_analysis')):
    # bivariate_analysis(data_test, var1, var2, var3)
