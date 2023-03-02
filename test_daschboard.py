4e6d4c95afad302e0ea4c11ff56973f6b41d5605
Assalli13 <124143755+Assalli13@users.noreply.github.com>
2023-03-02 17:01:47 +0100

    Update test_daschboard.py

--- test_daschboard.py
+++ test_daschboard.py
@@ -13,33 +13,30 @@ import json
 
 st.title('"Projet 7 - Implémentez un modèle de scoring"') 
 
-file1 = open('best_model_B_S.pkl', 'rb')
+file1 = open('final_model1.pkl', 'rb')
 model = pickle.load(file1)
 file1.close()
 
 
-data = pd.read_csv("X_valid_sample_sm")
-data_test = pd.read_csv("X_test_selected")
-#data['SK_ID_CURR'] = data['SK_ID_CURR'].astype(int)
-data = data.reset_index()
-data['SK_ID_CURR'] = data['index']
-data = data.drop('index', axis = 1)
+data = pd.read_csv("X_train_2")
+data_test1 = pd.read_csv("X_test_2")
+#data_test = data_test1.drop(['Unnamed: 0', 'TARGET'], axis =1)
+#st.write(data_test1)
 init = st.markdown("Réalisé par: Mohamed Assali")
-liste_id = data['SK_ID_CURR'].tolist()
+#liste_id = data['SK_ID_CURR'].tolist()
 #--------------------------------------------------------------------------------------------------------------------------------------------#
-
-#y_pred = model.predict(data_test.drop('Unnamed: 0', axis =1))
-prediction_test = model.predict_proba(data_test.drop('Unnamed: 0', axis =1))
+data_test = data_test1.drop(['Unnamed: 0', 'TARGET'], axis =1)
+prediction_test = model.predict_proba(data_test)
 y_pred_data_test = (prediction_test > 0.1)
 y_pred_data_test = np.array(y_pred_data_test > 0) * 1
-data_test['TARGET'] = list(prediction_test[:, 1])
-st.write(data_test)
+data_test['TARGET'] = list(1-prediction_test[:, 0])
+#st.write(data_test)
 #--------------------------------------------------------------------------------------------------------------------------------------------#
 y_pred = model.predict(data.drop('Unnamed: 0', axis =1))
 prediction = model.predict_proba(data.drop('Unnamed: 0', axis =1))
 y_pred_test = (prediction > 0.1)
 y_pred_test = np.array(y_pred_test > 0) * 1
-#st.write(data)
+#st.write(data_test1)
 #st.write(pd.DataFrame({
     #'Id': data['SK_ID_CURR'],
     #'pred': list(y_pred_test),
@@ -48,16 +45,10 @@ data['TARGET'] = list(prediction[:, 1])
 
 if(st.button('Get info')):
 
-    st.write(data.head(10))
-    client = data[data['SK_ID_CURR']==1]
-    # Afficher les données filtrées
-    #st.write(client)
-        # Afficher la valeur de la colonne 'TARGET' pour le client sélectionné
-    score_client = client['TARGET']
-    #st.write(score_client)
+    st.write(data_test1)
     
     def pie_chart(thres):
-        percent_sup_seuil = 100 * (data['TARGET'] > thres).sum() / data.shape[0]
+        percent_sup_seuil = 100 * (data_test['TARGET'] > thres).sum() / data_test.shape[0]
         percent_inf_seuil = 100 - percent_sup_seuil
         d = {
          'col1': [percent_sup_seuil, percent_inf_seuil],
@@ -78,43 +69,62 @@ if(st.button('Get info')):
         st.plotly_chart(fig)
     pie_chart(thres = 0.15)
     
-client_id = st.number_input("Enter the client ID:")   
+AMT_credit = st.sidebar.number_input('AMT_CREDIT', -2.0, 7.0, step=0.0001, format="%.4f") 
+income = st.sidebar.number_input('AMT_ANNUITY',  -2.0, 10.0,step=0.00001, format="%.4f")
+age = st.sidebar.number_input('DAYS_BIRTH', -4.0, 4.0, step=0.0001, format="%.4f")
+ext_source_1 = st.sidebar.number_input('EXT_SOURCE_1', -4.0, 4.0, step=0.0001, format="%.4f")
+ext_source_2 = st.sidebar.number_input('EXT_SOURCE_2', -4.0, 4.0,step=0.0001, format="%.4f")
+ext_source_3 = st.sidebar.number_input('EXT_SOURCE_3', -4.0, 4.0, step=0.0001, format="%.4f")
+
 if st.button('Get Score'):
     def get_predict_of_id():
     #Get the client ID from the user
+
+        data = {
+            'AMT_ANNUITY': income,
+            "AMT_CREDIT": AMT_credit,
+            'DAYS_BIRTH': age,
+            'EXT_SOURCE_1': income,
+            'EXT_SOURCE_2': ext_source_2,
+            'EXT_SOURCE_3': ext_source_3}
         
-
         
-        # data to send in the request body
-        id_client = {"SK_ID_CURR": client_id}
-
-            #if id_client:
-
-        # send the POST request
+        #response = requests.post("http://localhost:5000/predictByClientId", json=data)
         response = requests.post("https://flask-1.assalli13.repl.co/predictByClientId", json=id_client)
-        #response = requests.post("http://localhost:5000/predictByClientId", json=id_client)
 
         if response:
             
-            #response = requests.post("https://flask-1.assalli13.repl.co/predictByClientId", json=id_client)
+         
             #response = requests.post("http://assali.pythonanywhere.com//predictByClientId", json=id_client)
 
          # get the response data as a python object
             response_data = json.loads(response.text)
             response_data = response.json()
+            #st.write(response_data)
         else:
              print('NO')
 
-        return client_id, response_data
+        return data, response_data
+    def show_overview():
+            st.title("Risque")
+            risque_threshold = st.slider(label = 'Seuil de risque', min_value = 0.0,
+                            max_value = 1.0 ,
+                             value = 0.1,
+                             step = 0.1)
+            st.write(0.1)
+    show_overview()
 
 
     #Affichage du score cilent
     def gauge_chart(thres):
-        client_id, response_data = get_predict_of_id()
-        client_data = data.query("SK_ID_CURR == @client_id")
+        #client_id, response_data = get_predict_of_id()
+        data, response_data = get_predict_of_id()
+        
+        client_data = pd.DataFrame(data, index=pd.Index([0]))
+        #client_data = data.query("SK_ID_CURR == @client_id")
         st.write(client_data)
         if not client_data.empty:
-            #percent_sup_seuil = 100 * np.sum(response_data['prediction_proba']> thres) / client_data.shape[0]
+            percent_sup_seuil = 100 * np.sum(response_data['prediction_proba']> thres) / client_data.shape[0]
             st.write(response_data['prediction_proba'])
             percent_sup_seuil = 100*response_data['prediction_proba']
             if percent_sup_seuil<=100*thres:
@@ -176,22 +186,15 @@ if st.button('Get Score'):
             st.warning("No data available for this client id or the data is not valid")
     gauge_chart(thres = 0.15)
 
-    def show_overview():
-        st.title("Risque")
-        risque_threshold = st.slider(label = 'Seuil de risque', min_value = 0.0,
-                        max_value = 1.0 ,
-                         value = 0.15,
-                         step = 0.1)
-        st.write(0.15)
-    show_overview()
-    st.write(data)
+    
+    #st.write(data)
      
     def gauge_chart(thres):
         percent_sup_seuil = 100*(data['TARGET'] > thres).sum()/data.shape[0]
         fig = go.Figure(go.Indicator(
             mode = "gauge+number",
             value = percent_sup_seuil,
-            title = {"text": "Pourcentage de solvabil des clients di dataset"},
+            title = {"text": "Pourcentage de solvabilité des clients dans le dataset_test"},
             domain = {'x': [0, 1], 'y': [0, 1]},
             gauge = {'axis': {'range': [None, 100]},
                 'bar': {'color': "red"},
@@ -219,28 +222,36 @@ if st.button('Get Score'):
         st.pyplot(fig)
     hist_graph()
 
-var_selection = st.multiselect('Sélectionnez les variables à analyser', data_test.columns)
-if(st.button('Choice_feature')):
-    
-    def bivariate_analysis(data):
-        # Afficher un bouton pour sélectionner les variables à analyser
-        
-        if len(var_selection) < 2:
-            st.warning("Sélectionnez au moins deux variables pour l'analyse bivariée.")
-            return
-        
-        # Création des graphiques de dispersion entre la variable TARGET et les variables sélectionnées
-        figs = []
-        for var in var_selection:
-            fig = px.scatter(data, x=var, y='TARGET', title=f"{var} vs. TARGET")
-            figs.append(fig)
+def bivariate_analysis(data, var_selection):
+    if len(var_selection) < 2:
+        st.warning("Sélectionnez au moins deux variables pour l'analyse bivariée.")
+        return
 
-        # Affichage des graphiques avec Streamlit
-        for fig in figs:
-            st.plotly_chart(fig)
-            
-        return var_selection
-    bivariate_analysis(data_test)
+    # Calculer la matrice de corrélation entre les variables sélectionnées
+    corr = data[var_selection].corr()
+
+    # Créer la heatmap de la matrice de corrélation
+    fig = px.imshow(corr.values, x=corr.index, y=corr.columns, color_continuous_scale='RdBu')
+
+    # Spécifier le titre de la barre de couleur
+    fig.update_layout(coloraxis_colorbar_title='Corrélation')
+    
+
+    # Afficher la heatmap avec Streamlit
+    st.plotly_chart(fig)
+
+# Sélection des variables à analyser
+var_selection = st.multiselect('Sélectionnez les variables à analyser', data_test1.columns)
+
+# Bouton pour lancer l'analyse bivariée
+if st.button('Analyse bivariée'):
+    bivariate_analysis(data_test1, var_selection)
+
+
+
+        
+
+     
 
 
 
